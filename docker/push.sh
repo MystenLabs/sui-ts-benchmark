f95935ed87c469f25a0a8007ac3a50c831f59e5f
Jk Jensen <jk@mystenlabs.com>
2024-07-26 13:28:30 -0400

    make more robust

--- docker/push.sh
+++ docker/push.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-if [ -z "$1" ]
+source docker/versions.sh > /dev/null 2>&1
+
+VERSION="$1"
+if [ -z "$VERSION" ]
 then
-    echo "Please include the version number e.g. push.sh 10"
-    exit 1
+    echo "No version number provided, trying to get the next version number from the latest tag"
+    ((VERSION=$(get_versions | head -n1 | sed 's/^v//') + 1))
+    echo "Using $VERSION as the version number"
 fi
-export VERSION="$1"
 docker build --platform linux/amd64 -t us-central1-docker.pkg.dev/cryptic-bolt-398315/sui-ts-benchmark/sui-ts-benchmark:v$VERSION -f docker/Dockerfile .
 docker push us-central1-docker.pkg.dev/cryptic-bolt-398315/sui-ts-benchmark/sui-ts-benchmark:v$VERSION

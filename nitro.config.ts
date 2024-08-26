1e150923897a7f73608edf2c183fc2aaba171666
Mark Logan <mark@mystenlabs.com>
2024-08-26 10:13:29 -0700

    Revert "wip"
    This reverts commit 7967c81f85f33530c174715e88ae3cef22d610c7.

--- nitro.config.ts
+++ nitro.config.ts
@@ -10,10 +10,8 @@ export default defineNitroConfig({
 		tasks: true,
 	},
 	scheduledTasks: {
-		// Run simple transfter every minute
-		'* * * * *': ['execute:simple-transfer', 'report:balance'],
-		// Run shared-counter every minute, but offset by 30 seconds so that the logs are not comingled.
-		'30 * * * * *': ['execute:shared-counter'],
+		// Run `cms:update` task every minute
+		'* * * * *': ['execute:simple-transfer', 'execute:shared-counter', 'report:balance'],
 		// Run `report:ping` task every 10 seconds
 		'*/10 * * * * *': ['report:ping'],
 		// Smash coins every hour

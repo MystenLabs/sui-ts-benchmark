8612ae5b880b2d55b862af61d16b04f9854dbef7
Mark Logan <mark@mystenlabs.com>
2024-08-29 14:23:24 -0700

    Custom histogram buckets

--- utils/metrics.ts
+++ utils/metrics.ts
@@ -6,6 +6,9 @@ import {
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 import { Counter, Histogram, Meter } from '@opentelemetry/api';
 
+const TXN_HISTOGRAM_BUCKETS = [0, 250, 350, 450, 600, 750, 1000, 1250, 1500, 1750, 2000, 3000, 5000, 10000];
+const PING_HISTOGRAM_BUCKETS = [0, 10, 20, 40, 60, 80, 100, 150, 200, 250, 300, 400, 500];
+
 export class Instrumentation {
 	meter: Meter;
 	#histograms = new Map<string, Histogram>();
@@ -54,9 +57,11 @@ export class Instrumentation {
 		this.#gauges.set(name, value);
 	}
 
-	getHistogram(name: string) {
+	getHistogram(name: string, buckets: number[]) {
 		if (!this.#histograms.has(name)) {
-			this.#histograms.set(name, this.meter.createHistogram(name));
+			this.#histograms.set(name, this.meter.createHistogram(name, {
+				advice: { explicitBucketBoundaries: buckets },
+			}));
 		}
 
 		return this.#histograms.get(name)!;
@@ -66,7 +71,8 @@ export class Instrumentation {
 		const counter = this.getCounter(`${name}:calls`);
 		const success_counter = this.getCounter(`${name}:success`);
 		const error_counter = this.getCounter(`${name}:errors`);
-		const histogram = this.getHistogram(`${name}:duration`);
+		const buckets = name === 'report:ping' ? PING_HISTOGRAM_BUCKETS : TXN_HISTOGRAM_BUCKETS;
+		const histogram = this.getHistogram(`${name}:duration`, buckets);
 		// force counters to initialize if they haven't
 		counter.add(0);
 		success_counter.add(0);

616a72662e55ea28a4175299b6cde514814a085d
Mark Logan <103447440+mystenmark@users.noreply.github.com>
2025-03-18 12:32:23 -0700

    sanity logs

--- utils/executor.ts
+++ utils/executor.ts
@@ -23,6 +23,7 @@ let gasPrice: bigint | null = null;
 async function getGasPrice() {
 	if (!gasPrice) {
 		gasPrice = await suiClient.getReferenceGasPrice();
+		console.log("fetched gasPrice", gasPrice);
 	}
 
 	return gasPrice;
@@ -50,6 +51,7 @@ export function executeTransaction(
 			try {
 				return serialExecutor.executeTransaction(transaction);
 			} catch (error) {
+				console.log("error, clearing gasPrice");
 				gasPrice = null;
 				throw error;
 			}
